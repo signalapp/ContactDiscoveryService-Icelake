ccb3fb47f8705ab3bb90b7bab741995a8f6fb5f2
gram-signal <84339875+gram-signal@users.noreply.github.com>
2026-04-09 10:14:44 -0700

    Fix MSFT apt conflict.

--- c/docker/sgx_runtime_libraries.sh
+++ c/docker/sgx_runtime_libraries.sh
@@ -8,12 +8,15 @@ apt-get install -y \
       wget \
       software-properties-common \
       debian-archive-keyring \
-## apt-get install
-echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" | tee /etc/apt/sources.list.d/msprod.list
-echo "deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu jammy main" | tee /etc/apt/sources.list.d/sgx.list
+      ##
 
+# Install Microsoft production repo.
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+echo "deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu jammy main" | tee /etc/apt/sources.list.d/sgx.list
 wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | apt-key add -
-wget -qO - https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
 apt-get update
 apt-get -y install \
     libsgx-ae-epid=2.25.100.3-jammy1 \
